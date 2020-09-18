cc80148ee3130e864b3c71d2fdbcd78f88cb1085
helectron <helena.cortes@udea.edu.co>
2020-09-18 11:42:39 +0000

    better comment

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
-/*First line in C */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle\n");
