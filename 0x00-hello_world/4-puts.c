b50cf08b0875fc0dd68c68c4179503fda3811465
helectron <helena.cortes@udea.edu.co>
2020-09-18 11:30:04 +0000

    With Betty linter

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 /*First line in C */
-int main()
+int main(void)
 {
-	puts ("Programming is like building a multilingual puzzle");
-	return 0;
+	puts("Programming is like building a multilingual puzzle");
+	return(0);
 }
