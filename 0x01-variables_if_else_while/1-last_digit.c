e67f72864591b11e3052593e9f6aa9a6f53653b6
helectron <helena.cortes@udea.edu.co>
2020-09-21 19:32:08 +0000

    Fixing the 0

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,22 +10,23 @@
 
 int main(void)
 {
-	int n, mod;
+	int n;
+	int mod;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	mod = n % 10;
-	if (mod > 5)
-	{
-		printf("Last digit of %i is %i and is greater than 5\n", n, mod);
-	}
-		else if (mod < 6)
-		{
-			printf("Last digit of %i is %i and is less than 6\n", n, mod);
-		}
-		else if (mod == 0)
-		{
-			printf("Last digit of %i is %i and is 0\n", n, mod);
-		}
+	if (mod > 5)	
+{
+	printf("Last digit of %i is %i and is greater than 5\n", n, mod);
+}
+	else if (mod < 6)
+{
+	printf("Last digit of %i is %i and is less than \n", n, mod);
+}
+	else if (mod == 0)
+{
+	printf("Last digit of %i is %i and is 0\n", n, mod);
+}
 	return (0);
 }
