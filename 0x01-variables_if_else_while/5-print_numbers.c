d600e0b6f6669fc08e58684bc760fd2733b4a333
helectron <helena.cortes@udea.edu.co>
2020-09-22 20:52:36 +0000

    the c file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+*main - numbers of base 10 follow by a new line
+*return: 0 (sucess)
+*/
+
+int /*data type for the function, that will usually returns 0 if success */
+main(void) /*the name: main. This progam puts the digits in base 10 */
+
+{
+	int base10;
+
+	for (base10 = '0' ; base10 <= '9' ; base10++)
+	{
+		putchar(base10);
+	}
+
+	putchar(10);
+
+	return (0);
+}
