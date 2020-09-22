693f098972218cf00a85ca145d8da8ab16aa52ff
helectron <helena.cortes@udea.edu.co>
2020-09-22 15:51:41 +0000

    fixing style with betty

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 /**
- * Print a-z in lowercase
+ * Print a-z in lowercase with putchar
  * Return: 0 (success).
  */
-
-int /* the main function will usually returns a 0 if success*/ 
-main(void) /*this is the naime, in this case main*/
+int /* The main function will return a 0 if success*/
+main(void) /* This is the name, in this case main*/
 {
 	char c = 'a';
 
