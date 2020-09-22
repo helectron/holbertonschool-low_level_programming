20230bccb0ecf73986a5fef7916fa9c5b8afd0aa
helectron <helena.cortes@udea.edu.co>
2020-09-22 21:59:35 +0000

    digit numbers of base 10 without char variable

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+* main - Entry point of put digits from 0 in base 10.
+* Return: 0 (success).
+*/
+
+int /*data type for the function, that will usually returns 0 if success */
+main(void) /*the name: main. This progam puts the digits in base 10 */
+
+{
+	int base10;
+
+	for (base10 = '0' ; base10 <= '9' ; base10++)
+	{
+		putchar(base10);
+	}
+
+	putchar(10);
+
+	return (0);
+}
