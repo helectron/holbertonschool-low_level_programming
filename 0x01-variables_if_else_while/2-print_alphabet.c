c5812aa4c28ca2280fdba52f58316d3c3ffa20d3
helectron <helena.cortes@udea.edu.co>
2020-09-22 16:09:45 +0000

    last try for Betty

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
 /**
  * Print a-z in lowercase with putchar
  * Return: 0 (success).
  */
-int 
-main(void)
+
+int /*the main function will usually returns 0 if success */
+main(void) /*this is the namem in this case main*/
 {
 	char c = 'a';
 
 	for (c = 'a'; c <= 'z'; c++)
+	{
 		putchar(c);
+	}
 
 	putchar('\n');
 	return (0);
