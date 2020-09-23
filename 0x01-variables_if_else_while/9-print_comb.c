dee9d60577d56a0b87cb07395ed3459c9ed9863b
helectron <helena.cortes@udea.edu.co>
2020-09-23 00:19:46 +0000

    with the comma at the end

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n + '0');
+		if (n != 10)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
