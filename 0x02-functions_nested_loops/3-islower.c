3c8223540d22da7ee17edbe3895fa58f75177fe4
helectron <helena.cortes@udea.edu.co>
2020-09-27 18:27:41 +0000

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _islower - entry point for detecting lowercase letters
+ *Return: 1 if c is lowercase, 0 otherwise
+ */
+
+
+int _islower(int c)
+
+{
+if (c >= 97 && c <= 122)
+	return (1);
+{
+	return (0);
+}
+}
