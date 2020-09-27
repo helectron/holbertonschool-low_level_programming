79275ad33b642f9df801c03f797ea94d3b1709a1
helectron <helena.cortes@udea.edu.co>
2020-09-27 15:42:07 +0000

    alphabeth with _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * main - print the alphabet in lowercase with just 2 _putchar.
+ *
+ * Return: void.
+ */
+void print_alphabet(void)
+{
+	int c = 0;
+
+	for (c = 97; c <= 122; c++)
+	{
+		_putchar(c);
+	}
+	_putchar(10);
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,5 +1,8 @@
 #ifndef FILE_HOLBERTON
 #define FILE_HOLBERTON
 
+/* Prototypes */
+
 int _putchar(char c);
+void print_alphabet(void);
 #endif
