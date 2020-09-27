592007e3e3e445c4a0b7917fe507b1374012e3b8
helectron <helena.cortes@udea.edu.co>
2020-09-27 17:30:30 +0000

    alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ *print_alphabet_x10 - prints the alphabeth 10 times with just 2 _putchar
+ *
+ */
+
+void print_alphabet_x10(void)
+
+{
+int x = 0;
+int alph = 0;
+
+for (x = 0; x < 10; x++)
+{
+for (alph = 97; alph <= 122; alph++)
+_putchar (alph);
+}
+_putchar(10);
+}
