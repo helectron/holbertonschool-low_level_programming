80e248f3be3bd2eb84eff832b28ec4c727ff8b68
helectron <helena.cortes@udea.edu.co>
2020-09-27 17:41:28 +0000

    new line fixed

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,11 +11,13 @@ void print_alphabet_x10(void)
 int x = 0;
 int alph = 0;
 
-for (x = 0; x < 10; x++)
+for (x = 0; x <= 10; x++)
 {
-for (alph = 97; alph <= 122; alph++)
+for (alph = 97; alph <= 122; ++alph)
+{
 _putchar (alph);
 }
 alph = 122;
 _putchar(10);
+}
 }
