f5ed7d474488cc90937785acdaf8ea115afc2177
helectron <helena.cortes@udea.edu.co>
2020-09-28 12:12:45 +0000

    absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _abs - check the code for Holberton School students.
+ * @n: is an integer argument
+ * Return: 0 for succes.
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
