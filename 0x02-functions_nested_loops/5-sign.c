be6d18f74b48e06d70d67049cb65b89332aa1aab
helectron <helena.cortes@udea.edu.co>
2020-09-28 11:27:52 +0000

    put a sign to an argument

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * print_sign
+ *validate if the @n argument is negative, positive or zero
+ * Return: 1, 0 or -1 according to the sign of the argument
+ */
+int print_sign(int n)
+
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
