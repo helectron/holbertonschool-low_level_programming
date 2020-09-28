ff2b0e27e987f2407333eb3b484af0b5e4bed523
helectron <helena.cortes@udea.edu.co>
2020-09-28 22:03:19 +0000

    fixing the putchar

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,16 +6,17 @@
  * Return: 0 for succes.
  */
 
-int print_last_digit(int last)
+int print_last_digit(int x)
 
 {
-int l = last % 10;
+int r = x % 10;
 
-if (l >= 0)
+if (r >= 0)
+{
+_putchar(r + '0');
+return (r);
+}
 
-_putchar(l);
-return (l);
-
-_putchar(-l);
-return (-l);
+_putchar(-r + '0');
+return (-r);
 }
