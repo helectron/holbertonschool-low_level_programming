28b66b4906858e1d8ba4748c6940c657c6360999
helectron <helena.cortes@udea.edu.co>
2020-09-30 02:54:21 +0000

    gcc after changes

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 
 /**
- * reset_to_98 - updates the value it points
- * Return void
+ * reset_to_98 - main updates the value it points
+ * Return: void
  */
 
 void reset_to_98(int *n)
