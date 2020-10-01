6de69c89e4467d541021aaa91f30dfd32ecd7e57
helectron <helena.cortes@udea.edu.co>
2020-10-01 23:57:26 +0000

    add two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * add - add a+b
+ * @a: integer to add
+ * @b: integer to add
+ * Return: 0 for success
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+}
