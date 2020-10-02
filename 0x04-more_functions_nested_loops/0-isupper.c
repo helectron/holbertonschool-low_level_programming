6e74bf84a00e5f5613b4191f6f8aa3e6ca1878d8
helectron <helena.cortes@udea.edu.co>
2020-10-02 00:17:15 +0000

    more functions and nested loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _isupper - check if c is uppercase
+ * @c: integer param
+ * Return: 0 for succes
+ */
+
+
+int _isupper(int c)
+{
+
+	return (c >= 65 && c <= 90);
+}
