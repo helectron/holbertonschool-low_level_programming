48be671323974ed8ba4cef61d07bef40ecc9c1e2
helectron <helena.cortes@udea.edu.co>
2020-10-02 02:54:00 +0000

    code fixed

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -4,26 +4,28 @@
 /**
  * main - Entry point serie of numbers with two digits
  *
- * Return: Always 0 (Success)
+ * Return: 0 for success
  */
 int main(void)
 
 {
-	int n = 48, n2 = 48;
+	int n;
+	int n2;
 
-	for (n = 48; n < 57; n++)
-		for (n2 = 48; n2 > 57; n2++)
+	for (n = 48; n <= 57; n++)
 
+	{		
+		for (n2 = 48; n2 <= 57; n2++)
 		{
 			putchar(n);
 			putchar(n2);
-
-			if (n != 57 || n2 != 57)
+			if (n < 57 || n2 < 57)
 			{
 				putchar(',');
 				putchar(' ');
 			}
 		}
+	}
 	putchar(10);
 	return (0);
 }
