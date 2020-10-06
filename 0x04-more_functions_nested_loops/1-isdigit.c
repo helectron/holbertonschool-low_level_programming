9255de21dcb8040d0d774f3e8bcd95614f4cdd8a
helectron <helena.cortes@udea.edu.co>
2020-10-06 13:02:32 +0000

    syntaxis fixed

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- *_isupper - check the code for Holberton School students
+ *_isdigit - check the code for Holberton School students
  *@c: is an int parameter
  *Return 0
  */
@@ -8,5 +8,5 @@
 int _isdigit(int c)
 
 {
-	return(c = '0' && c <= '9');
+	return (c >= '0' && c <= '9');
 }
