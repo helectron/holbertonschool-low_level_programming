5debc3d13d279700f20c37ed3d322555ae93c5ec
helectron <helena.cortes@udea.edu.co>
2020-10-06 23:58:59 +0000

    lenght of a string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,20 @@
 #include "holberton.h"
 
 /**
+ *_strlen - the string´s lenght
  *
- *
- *
+ *@s: input char
+ *Return: lenght of a string
  */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		i++;
+	}
+	return (i);
+}
