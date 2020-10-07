e992294649dbc5fc8dcad481c831a1ebfc9408e5
helectron <helena.cortes@udea.edu.co>
2020-10-07 16:07:58 +0000

    print diagonal with loops

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - print diagonal line
+ * @n: int parameter number of times that prints
+ * Return: void
+ */
+
+
+void print_diagonal(int n)
+{
+	int a, b;
+
+	if (n < 1)
+		_putchar(10);
+	for (a = 1; a < n; a++)
+	{
+		for (b = 1; b < a; b++)
+		{
+			_putchar(' ');
+		}	
+		_putchar(92);
+		_putchar(10);
+	}	
+}
