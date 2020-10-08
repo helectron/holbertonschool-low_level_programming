5d933a55a122b51dc027a34f3e65b4cf920d0f4f
helectron <helena.cortes@udea.edu.co>
2020-10-08 16:03:13 +0000

    what a rough triangle

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ *main - prints the numbers from 1 to 100, followed by a new line.
+ *But for multiples of three print Fizz
+ *instead of the number and for the multiples of five print Buzz
+ *For numbers which are multiples of both three and five print FizzBuzz
+ *Return: 0 for success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%i", i);
+		if (i < 100)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
