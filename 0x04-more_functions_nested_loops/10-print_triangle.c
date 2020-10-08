8153752f36254d7c28ecf4b7e7739b3a31ca87aa
helectron <helena.cortes@udea.edu.co>
2020-10-08 16:01:20 +0000

    what a rough triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ *print_triangle - prints a triangle
+ *@size: size of the triangle
+ *
+ *Return: no
+ */
+
+void print_triangle(int size)
+
+{
+	int i, j, h;
+	
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+		for (j = size - 1; j > 0; j--)
+		{
+			_putchar(32);
+		}
+		for (h = 1; h <= i; h++)
+		{
+			_putchar(35);
+		}		
+		_putchar(10);
+		}
+	}
+	else
+	{
+		_putchar(10);
+	}
+}
