295c0ba489f04406f29e362a42c606c36a41eb2c
helectron <helena.cortes@udea.edu.co>
2020-10-08 16:29:52 +0000

    fixed code

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,19 +11,19 @@ void print_triangle(int size)
 
 {
 	int i, j, h;
-	
+
 	if (size > 0)
 	{
 		for (i = 1; i <= size; i++)
 		{
-		for (j = size - 1; j > 0; j--)
+			for (j = size - i; j > 0; j--)
 		{
 			_putchar(32);
 		}
 		for (h = 1; h <= i; h++)
 		{
 			_putchar(35);
-		}		
+		}
 		_putchar(10);
 		}
 	}
