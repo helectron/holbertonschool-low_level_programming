ed2ad39c6ba773e45cab925b77d22014a360f25e
helectron <helena.cortes@udea.edu.co>
2020-10-16 02:46:05 +0000

    function that allocates memory using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * malloc_checked - using malloc to allocate memory 
+ *@b: bytes allocate 
+ *Return: pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+	return (p);
+}
---
+++ 0x0C-more_malloc_free/holberton.h
@@ -0,0 +1,10 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+
+#endif
