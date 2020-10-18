b55c660510fcc593e668ffbd874d90effbd024f3
helectron <helena.cortes@udea.edu.co>
2020-10-18 21:58:59 +0000

    First, solve the problem. Then, write the code

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - this function copy the string pointed to by src
+ * @dest: destination of the copy
+ * @src: source to the copy
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int x;
+
+	for (x = 0; src[x] != '\0'; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[x] = '\0';
+	return (dest);
+}
