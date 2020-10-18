ecb5acd55660460f4576c37df8d1851734b1d94a
helectron <helena.cortes@udea.edu.co>
2020-10-18 21:07:38 +0000

    rev string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -3,9 +3,21 @@
 /**
  * print_rev - prints string
  * @s: input
- * Return: no 
+ * Return: no
  */
 
 void rev_string(char *s)
 {
+	int i;
+	int j;
+	int largo = strlen(s);
+	char a;
+
+	for (i = 0, j = largo - 1; i < j; i++, j--)
+	{
+		a = s[i];
+		s[i] = s[j];
+		s[j] = a;
+	}
 }
+
