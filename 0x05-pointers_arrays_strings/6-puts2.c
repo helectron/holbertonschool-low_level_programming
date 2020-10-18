7ee1dc6fcb9d61338576c211aa43dbcffa41b7f9
helectron <helena.cortes@udea.edu.co>
2020-10-18 21:27:19 +0000

    prints every other char or a string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,8 +1,20 @@
 #include "holberton.h"
 
 /**
- *
- *
- *
+ * puts2 - print selected chars from a string
+ *@str: string to print, input
+ *Return: void
  */
 
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if ((a % 2) == 0)
+			_putchar(str[i]);
+	}
+	_putchar(10);
+}
