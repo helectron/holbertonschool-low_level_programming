d7c8f3ade4d894728e0ee532a680a2d9f8c3eaa1
helectron <helena.cortes@udea.edu.co>
2020-10-20 03:14:40 +0000

    prototype addition

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,21 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
+/**
+ * struct dog - struct for dog info
+ * @name: dog´s name
+ * @age: dog´s age
+ * @owner: dog´s owner
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
