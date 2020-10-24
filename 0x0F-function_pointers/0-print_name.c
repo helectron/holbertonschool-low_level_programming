e141551f5583956bd0ee391f4972679304bf2043
helectron <helena.cortes@udea.edu.co>
2020-10-24 01:17:28 +0000

    what´s my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * print_name - output with the name
+ * @name: name to print
+ * @f: function to print with
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,7 @@
+#ifndef __FUNCTION_POINTERS_H__
+#define __FUNCTION_POINTERS_H__
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+
+#endif
