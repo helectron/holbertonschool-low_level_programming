92f319d2f56c2fa1b1fc0fa9761a901ef565fb68
helectron <helena.cortes@udea.edu.co>
2020-10-30 00:05:08 +0000

    add variadic function

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all -
+ *@n: mumber of parameters
+ * Return: adding.
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0;
+	unsigned int x;
+	va_list list_num;
+
+	va_start(list_num, n);
+
+	if (n == 0)
+		return (0);
+	{
+		for (x = 0; x < n; x++)
+		{
+			sum += va_arg(list_num, int);
+		}
+		va_end(list_num);
+	}
+	return (sum);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,8 @@
+#ifndef __VARIADIC_FUNCTIONS__
+#define __VARIADIC_FUNCTIONS__
+#include <stdarg.h>
+#include <stdio.h>
+
+int sum_them_all(const unsigned int n, ...);
+
+#endif
