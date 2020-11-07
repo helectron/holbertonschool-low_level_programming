2a1ac97c94f2ce9552302db219fdf1af9f57e1d8
helectron <helena.cortes@udea.edu.co>
2020-11-07 12:07:17 +0000

    always with style

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,33 +1,33 @@
 #include <stdio.h>
 
 /**
- * main - a program that prints all possible different combinations of three digits.
+ * main - prints all possible different combinations of three digits.
  * Return: 0 for success
 */
 int main(void)
 {
-  int a; 
-  int b; 
-  int c;
+	int a;
+	int b;
+	int c;
 
-  for (a = 48; a <= 57 ; a++)
-    {
-      for (b = a + 1 ; b <= 57 ; b++) /*b is one added to a*/
+for (a = 48; a <= 57 ; a++)
 	{
-	  for (c = b + 1 ; c <= 57 ; c++)
-	    {
-	      putchar (a);
-	      putchar (b);
-	      putchar (c);
-	      if (a == 55 && b == 56 && c == 57)
+	for (b = a + 1 ; b <= 57 ; b++) /*b is one added to a*/
+	{
+		for (c = b + 1 ; c <= 57 ; c++)
 		{
-		  putchar (10);/* put a newline*/
-		  break;
+			putchar (a);
+			putchar (b);
+			putchar (c);
+			if (a == 55 && b == 56 && c == 57)
+		{
+			putchar (10);/* put a newline*/
+			break;
 		}
-	      putchar (44); /*print a comma*/
-	      putchar (32); /*put a space*/
-	    }
+			putchar (44); /*print a comma*/
+			putchar (32); /*put a space*/
+		}
 	}
-    }
-  return (0);
+	}
+	return (0);
 }
