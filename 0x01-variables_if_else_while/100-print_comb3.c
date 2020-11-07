6749f567d1c853cd39e126ea8505a37061655a78
helectron <helena.cortes@udea.edu.co>
2020-11-07 11:35:36 +0000

    loop through 2 variables printing digits combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point, print a combinations of 2 digits
+ * Return: 0 for success
+ * */
+
+int main(){
+	int a;
+	int b;
+
+	for (a = 48; a <= 57; a++) /*loop until a i equal to 9, same as 57 in ASCII */
+	{
+		for (b = a + 1; b<= 57; b++) /*b is one more than a*/
+		{
+			putchar(a);
+			putchar(b);
+			if (a == 56 && b == 57)
+				putchar(10);/*print a new line if the if condition satisfy */
+			putchar(','); /*put a comma */
+			putchar(32); /*print a space */
+			
+		}
+	}
+	return (0);
+}
