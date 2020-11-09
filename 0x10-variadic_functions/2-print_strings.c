c57ebb47167f412ef2fc1c512bb9e4aae6f1c75d
helectron <helena.cortes@udea.edu.co>
2020-11-09 02:48:40 +0000

    chechking if string to be printed is not NULL

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -26,7 +26,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("(nil)");
 		else
 			printf("%s", str);
-		if (i != (n - 1))
+		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
 	printf("\n");
