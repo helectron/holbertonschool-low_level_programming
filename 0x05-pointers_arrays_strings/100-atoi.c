9a36b37a4ee9bc8f620bb65452e1f86c31df0b84
helectron <helena.cortes@udea.edu.co>
2020-11-09 00:08:32 +0000

    sign correction

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * _atoi - converts the initial portion of the string to int. 
+ * _atoi - converts the initial portion of the string to int
  * @s: pointer to a string to be converted to an integer
  * Return: integer
  */
@@ -14,14 +14,12 @@ int _atoi(char *s)
 	int sign = 1;  /*Initialize sign as a positive*/
 	int i = 0;  /*Initialize index of first digit*/
 	int r;
-	
+
 	for (r = 0; s[r] != '\0'; r++)
 	{
-		if (s[0] == '-')  /*If number is negative update the sign */
-		{
-			sign = -1;
-			i++;
-		}
+		if (s[i] == '-')  /*If number is negative update the sign */
+		sign *= -1;
+		
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			while (s[i] >= '0' && s[i] <= '9')
