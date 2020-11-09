0b64c66c91c1f7fa9d8f4d4efadaab42937cba45
helectron <helena.cortes@udea.edu.co>
2020-11-09 00:25:09 +0000

    bug fixed

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,8 +13,8 @@ void more_numbers(void)
 	{
 		for (i = 0; i <= 14; i++)
 		{
-			if (i > 10)
-			_putchar(i / 10 + '0');
+			if (i >= 10)
+				_putchar(i / 10 + '0');
 			_putchar(i % 10 + '0');
 		}
 		_putchar(10);
