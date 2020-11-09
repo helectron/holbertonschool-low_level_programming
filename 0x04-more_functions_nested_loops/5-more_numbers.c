aa2e6ac7de4e8565772df54d4f71b3b686ce2009
helectron <helena.cortes@udea.edu.co>
2020-11-09 00:22:14 +0000

    a new cycle needed

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,16 +1,22 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
+ * more_numbers - print 10 times the numbers from 0 to 14
  * Return: no.
  */
 void more_numbers(void)
 {
 	int i;
-	for (i = 0; i < 15; i++)
+	int j;
+
+	for (j = 0; j <= 9; j++)
 	{
-		_putchar(i);	
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 10)
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+		}
 		_putchar(10);
 	}
 }
