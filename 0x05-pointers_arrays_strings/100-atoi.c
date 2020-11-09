48657df2922faaf7c746c42b4523e876233ba527
helectron <helena.cortes@udea.edu.co>
2020-11-09 00:04:01 +0000

    converts the initial portion of the string to int

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+
+/**
+ * _atoi - converts the initial portion of the string to int. 
+ * @s: pointer to a string to be converted to an integer
+ * Return: integer
+ */
+
+
+int _atoi(char *s)
+
+{
+	int res = 0;
+	int sign = 1;  /*Initialize sign as a positive*/
+	int i = 0;  /*Initialize index of first digit*/
+	int r;
+	
+	for (r = 0; s[r] != '\0'; r++)
+	{
+		if (s[0] == '-')  /*If number is negative update the sign */
+		{
+			sign = -1;
+			i++;
+		}
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			while (s[i] >= '0' && s[i] <= '9')
+			{
+				res = ((s[i] - '0') * sign) + res * 10;
+				i++;
+			}
+			break;
+		}
+		i++;
+	}
+	return (res);
+}
+
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,5 +1,7 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+#include <stdio.h>
+#include <stdlib.h>
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -12,5 +14,6 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 
 #endif
