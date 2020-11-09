176e92eb911e8239c6dc59c690b0b29d88cd16c9
helectron <helena.cortes@udea.edu.co>
2020-11-09 02:53:22 +0000

    checking if string to be printed is not NULL

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -13,11 +13,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	char *str;
 	va_list list;
 
-	if (separator == NULL)
-	{
-		separator = "";
-	}
-
 	va_start(list, n);
 	for (i = 0; i < n; i++)
 	{
