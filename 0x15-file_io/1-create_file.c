3761caadf6ad74d4c14d4ea9d534469d19d262c3
helectron <helena.cortes@udea.edu.co>
2020-11-11 21:05:26 +0000

    new line at the end of the file for style

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content == NULL) /*if text content is NULL create an empty file*/
 		return (1);
-		
+
 	for (count = 0; text_content[count]; count++) /*lenght of the text content*/
 		;
 
@@ -31,6 +31,3 @@ int create_file(const char *filename, char *text_content)
 	close(f_i);
 	return (1);
 }
-
-
-
