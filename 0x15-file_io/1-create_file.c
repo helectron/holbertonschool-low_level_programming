14ee0fc7ef8d214c11a9777a00b703b0b7976769
helectron <helena.cortes@udea.edu.co>
2020-11-11 20:41:50 +0000

    close at the end

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,26 +11,26 @@ int create_file(const char *filename, char *text_content)
 	int f_i, f_o;
 	int count = 0;
 
-	/*check errors*/
-	if (filename == NULL)
+	if (filename == NULL) /*check errors. if filename is NULL return -1*/
 		return (-1);
 
-	f_i = open(filename, O_RDWR | O_CREAT | O_TRUNC, 600); /*read*/
+	/*read. if the file already exists, truncate it */
+	f_i = open(filename, O_RDWR | O_CREAT | O_TRUNC, 600);
 
 	if (f_i == -1)
 		return (-1);
-	if (text_content == NULL)
+	if (text_content == NULL) /*if text content is NULL create an empty file*/
 		return (1);
 
 	for (count = 0; text_content[count]; count++) /*lenght of the text content*/
 		;
 
 	f_o = write(f_i, text_content, count); /*write*/
-	close(f_i);
 	if (f_o == -1)
 	{
 		return (-1);
 	}
+	close(f_i);
 	return (1);
 }
 
