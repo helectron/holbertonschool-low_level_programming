9bcea353b873116ce0dcec200e6c0d34960f6501
helectron <helena.cortes@udea.edu.co>
2020-11-22 17:42:46 +0000

    the length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ *_strlen_recursion - returns the length of a string.
+ *Return: number of bytes of the string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s == '\0')
+		return (0);
+
+	else
+	{
+		i = _strlen_recursion(s + 1);
+		i++;
+	}
+	return (i);
+}
+
