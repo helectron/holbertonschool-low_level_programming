4336bb35dcbe15da855671d78e1b760cf30a3bbf
helectron <helena.cortes@udea.edu.co>
2020-11-22 17:45:20 +0000

    description for variable s

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,7 +1,8 @@
 #include "holberton.h"
 
 /**
- *_strlen_recursion - returns the length of a string.
+ *_strlen_recursion - returns the length of a string
+ *@s: char string
  *Return: number of bytes of the string
  */
 
