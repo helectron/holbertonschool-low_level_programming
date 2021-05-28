c3b476e0f3f09efce79b49e96f9389a5c48aae0c
helectron <helena.cortes@udea.edu.co>
2021-05-28 18:10:17 -0500

    fixing details and betty style

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,26 +7,28 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-hash_table_t *hash_new = NULL;
-hash_node_t *head = NULL;
-/*memory allocation for the struct */
-hash_new = malloc(sizeof(hash_table_t));
-if (hash_new == NULL)
-    return (NULL);
+	hash_table_t *hash_new = NULL;
+	hash_node_t *head = NULL;
+	unsigned long int i = 0;
 
-/*memory allocation for the array member of the struct*/
-hash_new->size = size;
-hash_new->array = malloc(sizeof(head) * size);
-if (hash_new->array == NULL)
-{
-    free(hash_new);
-    return (NULL);
-}
-unsigned int i = 0;
-while (i < size)
-{
-    hash_new->array[i] = NULL;
-    i++;
-}
-return (hash_new);
+    /*memory allocation for the struct */
+	hash_new = malloc(sizeof(hash_table_t));
+	if (hash_new == NULL)
+	{
+		return (NULL);
+	}
+    /*memory allocation for the array member of the struct*/
+	hash_new->size = size;
+	hash_new->array = malloc(sizeof(head) * size);
+	if (hash_new->array == NULL)
+	{
+		free(hash_new);
+		return (NULL);
+	}
+	while (i < size)
+	{
+		hash_new->array[i] = NULL;
+		i++;
+	}
+	return (hash_new);
 }
