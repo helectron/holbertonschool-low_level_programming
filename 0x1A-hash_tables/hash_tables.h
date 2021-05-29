42a06aa0e28a83605e8faf3293ed96e43facb879
helectron <helena.cortes@udea.edu.co>
2021-05-28 19:29:02 -0500

    module for smaller number

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,9 +1,8 @@
-#ifndef __HASH_TABLES__H
-#define __HASH_TABLES__H
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
-#include <stdio.h>
 
 /**
  * struct hash_node_s - Node of a hash table
@@ -15,9 +14,9 @@
  */
 typedef struct hash_node_s
 {
-char *key;
-char *value;
-struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -30,11 +29,12 @@ struct hash_node_s *next;
  */
 typedef struct hash_table_s
 {
-unsigned long int size;
-hash_node_t **array;
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
 
 #endif
