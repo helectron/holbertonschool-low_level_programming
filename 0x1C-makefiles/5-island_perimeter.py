1894cfd9e80202659c364bed41f55ddc91e2eb0f
helectron <helena.cortes@udea.edu.co>
2021-07-04 20:27:10 -0500

    island perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+def island_perimeter(grid):
+    """ Returns the perimeter of an island representation in a matrix """
+    count = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                if i - 1 < 0 or grid[i - 1][j] == 0:
+                    count += 1
+                if j + 1 > (len(grid[i]) - 1) or grid[i][j + 1] == 0:
+                    count += 1
+                if i + 1 > (len(grid) - 1) or grid[i + 1][j] == 0:
+                    count += 1
+                if j - 1 < 0 or grid[i][j - 1] == 0:
+                    count += 1
+    return count
